82cb01468d37b89efa289806a44d8f07d429ac2c
Billy Hadlow <bhadlow@live.co.uk>
2021-08-04 21:17:41 +0100

    cli interface

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
-fn main()
+extern crate clap;
+
+use clap::{Arg, App, SubCommand};
+use tokio::{io::AsyncReadExt, net::TcpListener};
+
+#[tokio::main]
+async fn main()
 {
-    println!("Hello, world!");
+    let matches = App::new("Hypersquare")
+        .version("0.1.0")
+        .author("Billy Hadlow")
+        .about("")
+        .arg(Arg::with_name("test")
+             .short("t")
+             .long("test")
+             .value_name("T")
+             .help("This is a test command")
+             .takes_value(true))
+        .get_matches();
+
+    let test = matches.value_of("test").unwrap_or("no test entered");
+
+    println!("Test is: {}", test);
 }
